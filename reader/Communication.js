e5dad4675589d1b1b79caa58c31016d3393585b9
Tomás Vieira Caldas <tomas_v_caldas@hotmail.com>
2016-12-24 17:31:19 +0000

    Initial scene with nothing to show

--- reader/Communication.js
+++ reader/Communication.js
@@ -2,9 +2,14 @@ function getPrologRequest(requestString, onSuccess, onError, port)
 			{
 				var requestPort = port || 8081
 				var request = new XMLHttpRequest();
+				var board =  this;
 				request.open('GET', 'http://localhost:'+requestPort+'/'+requestString, true);
 
-				request.onload = onSuccess || function(data){console.log("Request successful. Reply: " + data.target.response);};
+
+				request.onload = onSuccess || function(data){
+					console.log("Request successful. Reply: " + data.target.response);
+
+				};
 				request.onerror = onError || function(){console.log("Error waiting for response");};
 
 				request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
